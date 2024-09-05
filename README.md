fb3d8034e35ff2c51754b417da476e431e976f48
Adam Aizal <adamaizal4477@gmail.com>
2024-09-05 12:21:35 +1200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Installation Guide
+1. Open folder on your IDE (Visual Studio Code is recommended)
+2. Download and install Node.js from this [link](nodejs.org "nodejs.org"). Choose the LTS version for stability.
+3. Run command in terminal to initialize Node.js project:```npm init -y```
+4. Execute script:```node revenueCalculator.js```
+5. You can hardcode the input values in the file.
---
+++ revenueCalculator.js
@@ -0,0 +1,51 @@
+// revenueCalculator.js
+
+// Function to calculate renewal revenue
+function calculateRenewalRevenue(previousARR, currentARR, dealAmount) {
+    if (previousARR >= currentARR) {
+        if (dealAmount >= (previousARR - currentARR)) {
+            return previousARR - currentARR;
+        } else {
+            return dealAmount;
+        }
+    } else {
+        return 0; // No renewal revenue needed if this year's total is greater than last year's total
+    }
+}
+
+// Function to calculate new revenue
+function calculateNewRevenue(dealAmount, previousARR, currentARR) {
+    var renewalRevenue = calculateRenewalRevenue(previousARR, currentARR, dealAmount);
+    
+    if (previousARR < currentARR) {
+        return dealAmount;
+    } else if (dealAmount > (previousARR - currentARR)) {
+        return dealAmount - renewalRevenue; // Calculate new revenue when deal amount is greater than the difference
+    } else {
+        return 0; // No new revenue if deal amount doesn't cover the difference
+    }
+}
+
+// Example inputs
+const previousARR = 10000; //2023 ARR
+const currentARR = 8000;  //2024 ARR
+const dealAmount = 4000;     //New 2024 Deal Amount
+
+
+// Calculate renewal and new revenue
+const renewalRevenue = calculateRenewalRevenue(previousARR, currentARR, dealAmount);
+const newRevenue = calculateNewRevenue(dealAmount, previousARR, currentARR);
+
+// Print results to console
+console.log("Inputs:");
+console.log("");
+console.log("2023 ARR:", previousARR);
+console.log("2024 ARR:", currentARR);
+console.log("2024 Amendment:", dealAmount);
+console.log("");
+console.log("");
+console.log("Outputs:");
+console.log("");
+console.log("Renewal Revenue:", renewalRevenue);
+console.log("New Revenue:", newRevenue);
+console.log("");
